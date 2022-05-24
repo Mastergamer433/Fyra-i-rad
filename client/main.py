8f9e2ab055e1c4d1fdba647f33417921a2039322
Mastergamer433 <teknikgillaren@gmail.com>
2022-05-24 13:40:41 +0200

    login beginning

--- client/main.py
+++ client/main.py
@@ -1,40 +1,13 @@
 import pygame
 from gameInfo import GameInfo
-
+from loop import gameLoop
 
 def main():
     """The main function"""
     done = False
     game_info = GameInfo(630, 630)
-    while not done:
-        # Varje sekund kör:
-        # netSend(UP_MESSAGE)
-        # tryTimes=0
-        # while netReceive() != UP_MESSAGE:
-        #   if(tryTimes==3):
-        #       Visa att klienten har tappat
-        #       anslutning
-        #       break;
-        #   netSend(UP_MESSAGE)
-        #   tryTimes+=1
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-        game_info.WIN.fill((255, 255, 255))
-        pygame.draw.rect(
-            game_info.WIN,
-            (0, 0, 255),
-            pygame.Rect(
-                0,
-                game_info.circleSizeHeight,
-                game_info.WIDTH,
-            )
-        )
-        game_info.WIN.blit(game_info.board, (0, game_info.circleSizeHeight))
-        pygame.display.flip()
-        game_info.clock.tick(60)
-    pygame.quit()
-
+    while not game_info.done:
+        gameLoop(game_info)
 
 if __name__ == "__main__":
     main()
